92435f4ca0b517b60ecfd64cf9724f71adadc845
Kim Santiago <65526784+taiga72@users.noreply.github.com>
2026-07-22 12:20:50 +0800

    Added capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.taiga72.calorietracker',
+  appName: 'Calorie Tracker',
+  webDir: 'dist'
+};
+
+export default config;
