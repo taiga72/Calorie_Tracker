9386da62491b289a362f1fcaae57dfecc01406df
Kim Santiago <65526784+taiga72@users.noreply.github.com>
2026-07-22 18:28:57 +0800

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { inject } from '@vercel/analytics';
-import { injectSpeedInsights } from '@vercel/speed-insights';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import App from './App.tsx';
 import './index.css';
-
-// Initialize Vercel Analytics & Speed Insights
-inject();
-injectSpeedInsights();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
